2ec6ac6e6417e1127cb0edc643ae91dce0742abc
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-01-16 16:08:46 -0300

    [add]: possible moviments

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,6 +1,6 @@
 ﻿namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
 
         public Posicao posicao { get; set; }
@@ -20,5 +20,7 @@
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
