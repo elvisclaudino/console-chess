5cd538048c818cb6a42a05e725afbf0295949b17
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-01-16 14:06:24 -0300

    [add]: chess position

---
+++ xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,19 @@
+﻿namespace xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha  = linha;
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
