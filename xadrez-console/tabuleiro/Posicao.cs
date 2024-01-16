c3c14713eee88c8f0b127d8a768e3aa5a4018535
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-01-16 13:16:16 -0300

    [add] position class

---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,19 @@
+﻿namespace tabuleiro
+{
+    internal class Posicao
+    {
+        public int linha { get;  set; }
+        public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna  = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha 
+        }
+    }
+}
